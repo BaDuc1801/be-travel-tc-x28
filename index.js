640fe6e7bf86f2f84fd435649affc36a1f1d8c89
BaDuc1801 <minhduc180104@gmail.com>
2024-11-04 21:51:58 +0700

    test

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const app = express();
 
 app.use("/destinations", destiRouter);
 
+app.get("/", (req, res)=>{
+  res.status(200).json({message: "hello"})
+})
+
 app.listen(8080, () => {
   console.log("Server is running!");
 });
