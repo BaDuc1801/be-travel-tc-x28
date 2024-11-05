d37dcf77d6a89b8447e312b864c29a98cad1169c
BaDuc1801 <minhduc180104@gmail.com>
2024-11-05 22:07:53 +0700

    fix

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors"
 import destiRouter from "./routes/destiRouter.js";
 await mongoose.connect(process.env.MONGOCONNECT);
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
 app.use("/destinations", destiRouter);
 
