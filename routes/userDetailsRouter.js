6238fdf7086f98d70de5778a9d08533d7e5f9445
LamPhamNgoc1310 <phamngoclam1310@gmail.com>
2024-11-08 20:13:35 +0700

    docs: updateuser schema

--- routes/userDetailsRouter.js
+++ routes/userDetailsRouter.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import userDetailsController from '../controller/userDetailsController.js';
+import multer from 'multer';
 
 const userDetailsRouter = express.Router()
 
@@ -7,4 +8,5 @@ const storage = multer.memoryStorage();
 
 userDetailsRouter.get('/:user', userDetailsController.getAllUsers)
 userDetailsRouter.post('/create', userDetailsController.createNewUser)
-export default userDetailsRouter;q
+
+export default userDetailsRouter;
