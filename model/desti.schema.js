5e401075a0e62fd8960b6fd0e6212d0f87783ae9
BaDuc1801 <minhduc180104@gmail.com>
2024-11-12 22:27:00 +0700

    update destnpm run dev

--- model/desti.schema.js
+++ model/desti.schema.js
@@ -5,8 +5,12 @@ const destiSchema = new mongoose.Schema({
     city: String,
     description: String,
     img: String,
+    coordinates: {
+        type: [Number], 
+        index: '2dsphere' 
+    }
 })
 
 const destiModel = mongoose.model('destinations', destiSchema);
 
-export default destiModel
+export default destiModel;
