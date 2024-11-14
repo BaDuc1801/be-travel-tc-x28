6793fab1217b10e0cac65b5a415a39c58d82a355
BaDuc1801 <minhduc180104@gmail.com>
2024-11-14 22:23:32 +0700

    update model city

--- model/desti.schema.js
+++ model/desti.schema.js
@@ -1,14 +1,15 @@
 import mongoose from "mongoose";
 
 const destiSchema = new mongoose.Schema({
-    destiName: String,
-    city: String,
-    description: String,
-    img: String,
-    coordinates: {
-        type: [Number]
-    }
-})
+  destiName: String,
+  city: String,
+  description: String,
+  img: String,
+  coordinates: {
+    type: { type: String, enum: ['Point'], required: true },
+    coordinates: { type: [Number], required: true }
+  }
+});
 
 const destiModel = mongoose.model('destinations', destiSchema);
 
